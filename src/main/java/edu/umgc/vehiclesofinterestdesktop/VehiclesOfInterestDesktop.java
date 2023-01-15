788b97a21ce24a4f5cdcb2a92c387caec7a3d4d8
Viet <vietjul@gmail.com>
2023-01-15 13:52:27 -0500

    feat: Working code

---
+++ src/main/java/edu/umgc/vehiclesofinterestdesktop/VehiclesOfInterestDesktop.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package edu.umgc.vehiclesofinterestdesktop;
+
+/**
+ *
+ * @author viet
+ */
+public class VehiclesOfInterestDesktop {
+
+    public static void main(String[] args) {
+        MainScreen ms = new MainScreen();
+        ms.show();
+    }
+}
